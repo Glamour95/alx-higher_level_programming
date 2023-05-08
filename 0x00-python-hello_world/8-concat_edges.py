f0f2ebd9125584905043b2818871accc41615e9e
Glamour <glamour.maphangat@gmail.com>
2023-05-08 11:00:08 -0700

    updated

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
-        language that combines remarkable power with very clear syntax"
-str = str[39:67] + str[107:112] + str[:6]
-print(str)
+str1 = "object-oriented"
+str2 = "programming"
+str3 = "with"
+str4 = "Python"
+print("{} {} {} {}".format(str1[-11:], str2[0:10], str3, str4))
