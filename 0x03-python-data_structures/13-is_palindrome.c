f456524e45e3b92260cdc78040366180f0f0078f
Glamour <glamour.maphangat@gmail.com>
2023-05-16 11:22:47 -0700

    updated

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -3,6 +3,30 @@
 #include "lists.h"
 
 /**
+ * reverse_listint - reverses a linked list
+ * @head: pointer to pointer to the head of the list
+ *
+ * Return: pointer to the new head of the reversed list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prev = NULL;
+	listint_t *current = *head;
+	listint_t *next = NULL;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+
+	*head = prev;
+	return (*head);
+}
+
+/**
  * is_palindrome - checks if a singly linked list is a palindrome
  * @head: pointer to pointer to the head of the list
  *
@@ -12,52 +36,36 @@ int is_palindrome(listint_t **head)
 {
 	listint_t *slow = *head;
 	listint_t *fast = *head;
-	listint_t *prev = NULL;
-	listint_t *next = NULL;
+	listint_t *mid = NULL;
 	listint_t *second_half = NULL;
-	int is_palindrome = 1;
 
 	if (*head == NULL || (*head)->next == NULL)
-		return (is_palindrome);
+		return (1);
 
 	while (fast != NULL && fast->next != NULL)
 	{
 		fast = fast->next->next;
-		prev = slow;
+		mid = slow;
 		slow = slow->next;
 	}
 
 	if (fast != NULL)
 	{
-		second_half = slow->next;
-		slow->next = prev;
-	}
-	else
-	{
-		second_half = slow;
-		prev->next = NULL;
+		mid = slow;
+		slow = slow->next;
 	}
 
-	while (second_half != NULL)
-	{
-		next = second_half->next;
-		second_half->next = prev;
-		prev = second_half;
-		second_half = next;
-	}
+	second_half = slow;
+	mid->next = NULL;
+	reverse_listint(&second_half);
 
-	second_half = prev;
-	while (second_half != NULL)
+	while (*head != NULL && second_half != NULL)
 	{
 		if ((*head)->n != second_half->n)
-		{
-			is_palindrome = 0;
-			break;
-		}
+			return (0);
 		*head = (*head)->next;
 		second_half = second_half->next;
 	}
 
-	return (is_palindrome);
+	return (1);
 }
-
