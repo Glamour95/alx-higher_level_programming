391aaac5995f4f2bb6d89620c4ee7ea3c433d310
Glamour <glamour.maphangat@gmail.com>
2023-06-13 11:39:14 -0700

    output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,5 +3,12 @@
 
 def read_file(filename=""):
     """Reads and prints a text file (UTF8) to stdout"""
-    with open(filename, encoding="utf-8") as f:
-        print(f.read(), end="")
+    try:
+        with open(filename, encoding="utf-8") as f:
+            print(f.read(), end="")
+    except FileNotFoundError:
+        print(f"Error: File '{filename}' not found.")
+    except PermissionError:
+        print(f"Error: Permission denied for file '{filename}'.")
+    except Exception as e:
+        print(f"An error occurred while reading the file: {e}")
