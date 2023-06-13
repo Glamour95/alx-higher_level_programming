aedd24dc0d4a92485f89a63c365f28d55c3174e3
Glamour <glamour.maphangat@gmail.com>
2023-06-13 12:00:07 -0700

    input

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,10 +1,23 @@
+#!/usr/bin/python3
+ """
+    Returns a dictionary description with a simple data structure
+    for JSON serialization of an object
+    """
+
 def class_to_json(obj):
-    """Converts an instance of a Class to a dictionary representation for JSON serialization"""
-    if hasattr(obj, "__dict__"):
-        # Get the attributes of the object
-        attributes = obj.__dict__.copy()
-        # Remove any private attributes starting with '__'
-        attributes = {key: value for key, value in attributes.items() if not key.startswith('__')}
-        return attributes
-    else:
+    """
+    Returns a dictionary description with a simple data structure
+    for JSON serialization of an object
+    """
+    if isinstance(obj, dict):
+        return {key: class_to_json(value) for key, value in obj.items()}
+    elif isinstance(obj, list):
+        return [class_to_json(element) for element in obj]
+    elif isinstance(obj, str) or isinstance(obj, int) or isinstance(obj, bool):
         return obj
+    else:
+        attributes = {}
+        for attr in dir(obj):
+            if not attr.startswith("__") and not callable(getattr(obj, attr)):
+                attributes[attr] = class_to_json(getattr(obj, attr))
+        return attributes
