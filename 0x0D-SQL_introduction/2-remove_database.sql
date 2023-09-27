8d3c3fd2dbd5cc3a88d2ab1ab4dca5773c62dbc9
Glamour <118850729+Glamour95@users.noreply.github.com>
2023-09-27 13:42:30 +0200

    Delete 0x0D-SQL_introduction directory

--- 0x0D-SQL_introduction/2-remove_database.sql
+++
@@ -1,2 +0,0 @@
--- Lists all tables of a database.
-DROP DATABASE IF EXISTS hbtn_0c_0;
--- 0x0D-SQL_introduction/4-first_table.sql
+++
@@ -1,5 +0,0 @@
--- Deletes the database hbtn_0c_0.
-CREATE TABLE IF NOT EXISTS first_table (
-    id INT,
-    name VARCHAR(256)
-);
